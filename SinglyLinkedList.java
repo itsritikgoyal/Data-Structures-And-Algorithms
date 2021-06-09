8b9f695e53cb7a66936736355d26976109fbb74f
Ritik Goyal <69449533+itsritikgoyal@users.noreply.github.com>
2021-06-09 16:27:51 +0530

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -21,18 +21,31 @@ public class SinglyLinkedList {
     System.out.println(null);
   }
   
+  public int length(){    //  function to calculate the length of sll
+      if(head==null)
+          return 0;
+      int count = 0;
+      ListNode current = head;
+      while(current != null){
+          count++;
+          current = current.next;
+      }
+    return count;
+  }
+  
   public static void main(String[] args){
     
-    SinglyLinkedList ssl = new SinglyLinkedList();    // insertion elements in a linked list
-      ssl.head = new ListNode(10);
+    SinglyLinkedList sll = new SinglyLinkedList();    // insertion elements in a linked list
+      sll.head = new ListNode(10);
       ListNode second = new ListNode(1);
       ListNode third = new ListNode(7);
       ListNode fourth = new ListNode(8);
       
-      ssl.head.next = second;
+      sll.head.next = second;
       second.next = third;
       third.next = fourth;
     
       sll.display();  // display the elements 
+      System.out.println(sll.length());   // print the length 
   }
 }
