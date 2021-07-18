6519f8139f46d42fe91a373a9f753cb210788ee4
itsritikgoyal <goyalsahb1114@gmail.com>
2021-07-18 16:26:32 +0530

    dsljsfkl

--- Sll.java
+++ Sll.java
@@ -1,4 +1,5 @@
-public class Sll {
+public class Sll 
+{
     private ListNode head;
 
     public static class ListNode{
@@ -14,19 +15,19 @@ public class Sll {
     
     public void display(){
         ListNode temp = head;
-        while(temp.next != null){
-            System.out.println(temp.data);
+        while(temp != null){
+            System.out.print(temp.data + "-->");
             temp = temp.next;
         }
         System.out.println("null");
     }
     
     public int length(){
-        if(head.next==null)
+        if(head ==null)
             return 0;
         int count = 0;
         ListNode temp = head;
-        while(temp.next != null){
+        while(temp != null){
             temp = temp.next;
             count++;
         }
@@ -59,25 +60,76 @@ public class Sll {
     }
 
     public ListNode deleteFirst(){
-        
+        if(head == null){
+            return null;
+        }
+        ListNode temp  = head;
+        head  = head.next;
+        temp.next = null;
+        return temp;
     }
+
+    public ListNode deleteEnd(){
+        if( head == null || head.next == null ){
+            return head;
+        }
+        ListNode current = head;
+        ListNode previous = null;
+        while(current.next!=null){
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        return current;   
+
+    }
+
+    public void insert(int data, int position){
+        ListNode newnode = new ListNode(data);
+        if(position==1){
+            newnode.next = head;
+            head = newnode;
+        }
+        else{
+            ListNode previous = head;
+            ListNode current  = head;
+            int count  = 1;
+            while(count < (position-1)){
+                previous = previous.next;
+                count++;
+            }
+            current = previous.next;
+            previous.next = newnode;
+            newnode.next = current;
+            
+        }
+    }
+
+    public void delete(int position){
+        if(position == 1 ){
+            head = head.next;
+        }
+        else{
+            ListNode previous = head;
+            int count = 1;
+            while(count < position - 1){
+                previous = previous.next;
+                count++;
+            }
+            ListNode current = previous.next;
+            previous.next=current.next;
+        }
+    }
+
 
     public static void main(String[] args) {
         Sll sll = new Sll();
 
-        sll.head = new ListNode(10);
-        ListNode second = new ListNode(20);
-        ListNode third = new ListNode(20);
-
-        sll.head.next = second;
-        second.next = third;
-
-        sll.insertFirst(5);
-        sll.insertFirst(6);
-
-        sll.insertEnd(10);
-
-        sll.display(); // Display the Linked List
+        sll.insert(10, 1);
+        sll.insert(10, 1);
+        sll.insert(10, 3);
         System.out.println(sll.length()); // Print the length of linked list
+        sll.delete(1);
+        sll.display(); // Display the Linked List
     }
 }
